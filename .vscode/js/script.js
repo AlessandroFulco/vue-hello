e28e04ea501f9ecf4d728c0445551c6c1b77102a
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-05-24 14:53:18 +0200

    finalizzazione bonus

--- .vscode/js/script.js
+++ .vscode/js/script.js
@@ -2,5 +2,6 @@ let root = new Vue({
     el:'#container',
     data: {
         stringa: 'Hello, world!',
+        source: "https://media-exp1.licdn.com/dms/image/C4D0BAQH3BE0UnK2ndw/company-logo_200_200/0/1644402020387?e=2147483647&v=beta&t=Lr7FLr_PsSSRXsf4B8Hwb2BOQN0dr4m9fG181L8gaek",
     }
 });
